c5259d836e55b0c4a093505d28cba89eb9648492
nosadatech <nmakida2010@gmail.com>
2021-09-09 11:59:44 +0900

    Commentモデル、ルーティング、コントローラーのcreateアクション定義

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,12 @@
+class CommentsController < ApplicationController
+  def create
+    comment = Comment.create(comment_params)
+    redirect_to tweet_path(comment.tweet_id)
+  end
+  
+  private
+  def comment_params
+    params.require(:comment).permit(:text).merge(user_id: current_user.id, comment: params[:tweet_id])
+  end
+
+end
